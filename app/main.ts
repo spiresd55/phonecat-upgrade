ddcddef779424efa53330a9684cc9b2fdee09271
dmspires <dmspires@us.ibm.com>
2016-06-29 15:35:57 -0500

    changed phone list component from angular 1 to 2

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { UpgradeAdapter } from '@angular/upgrade';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { Phone } from './core/phone/phone.service';
+import { PhoneListComponent } from './phone-list/phone-list.component';
 
 let upgradeAdapter = new UpgradeAdapter();
 
@@ -9,6 +10,9 @@ upgradeAdapter.addProvider(Phone);
 
 angular.module('core.phone')
     .factory('phone', upgradeAdapter.downgradeNg2Provider(Phone));
+
+angular.module('phoneList')
+    .directive('phoneList', <angular.IDirectiveFactory> upgradeAdapter.downgradeNg2Component(PhoneListComponent));
 
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
 
--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,22 +1,54 @@
+import {Component} from '@angular/core';
 import {Phone, PhoneData} from '../core/phone/phone.service';
 
-class PhoneListController {
+@Component({
+  selector: 'phone-list',
+  templateUrl: 'phone-list/phone-list.template.html'
+})
+
+export class PhoneListComponent {
   phones: PhoneData[];
+  query: string;
   orderProp: string;
 
-  //Injects the Phone provider service
-  static $inject = ['phone'];
   constructor(phone: Phone) {
     phone.query().subscribe(phones => {
       this.phones = phones;
     });
     this.orderProp = 'age';
   }
+
+  getPhones(): PhoneData[] {
+    return this.sortPhones(this.filterPhones(this.phones));
+  }
+
+  private filterPhones(phones: PhoneData[]) {
+    if(phones && this.query) {
+      return phones.filter(phone => {
+        let name = phone.name.toLowerCase();
+        let snippet = phone.snippet.toLowerCase();
+        let search = this.query.toLowerCase()
+        return name.indexOf(search) >= 0 || snippet.indexOf(search) >= 0;
+      });
+    }
+    return phones;
+  }
+
+  private sortPhones(phones: PhoneData[]) {
+    if (phones && this.orderProp) {
+      return phones
+          .slice(0) // Make a copy
+          .sort((a, b) => {
+            if (a[this.orderProp] < b[this.orderProp]) {
+              return -1;
+            } else if ([b[this.orderProp] < a[this.orderProp]]) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+    }
+    return phones;
+  }
 }
-// Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: PhoneListController
-  });
+
